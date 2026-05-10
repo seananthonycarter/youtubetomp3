c7c6a1e487ed210ccbf45bef44bd1bb9935f6d27
Sean Carter <37186285+seananthonycarter@users.noreply.github.com>
2026-05-10 17:27:51 +0100

    Initial commit

---
+++ YouTubeToMP3/FfmpegWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeToMP3
+{
+    public static class FfmpegWrapper
+    {
+        public static void Convert(string inputPath, string outputPath)
+        {
+            var args = $"-i \"{inputPath}\" \"{outputPath}\"";
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = args,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            string result = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                throw new Exception($"FFmpeg failed: {result}");
+            }
+        }
+    }
+}
---
+++ YouTubeToMP3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using VideoLibrary;
+
+namespace YouTubeToMP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(args[0]);
+            var mp4path = $"{args[1]}\\{vid.FullName}";
+            var mp3path = mp4path.Replace("mp4", "mp3");
+            File.WriteAllBytes(mp4path, vid.GetBytes());
+            FfmpegWrapper.Convert(mp4path, mp3path);
+        }
+    }
+}
